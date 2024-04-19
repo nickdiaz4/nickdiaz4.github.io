b8fd5a5c41778854c3922f54f555713f14b4d8a9
jimenezn3 <nickjimenez2014@gmail.com>
2024-04-19 18:39:54 -0400

    content changes

--- portfolio/src/components/ContentCard.js
+++ portfolio/src/components/ContentCard.js
@@ -42,8 +42,12 @@ export default class ContentCard extends Component {
                     <Typography level="body-sm" fontFamily={"Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif"}>
                         I graduated from Winthrop University in 2023 with a B.S. in Computer Science.
                         I have an <a href='https://www.credly.com/badges/5fc2088a-1de2-4e8e-b962-8d1ca98a099d/linked_in_profile'>AWS Certified Cloud Practitioner Certification</a> as well as an <a href='https://www.credly.com/badges/fce1b49e-dd57-44f5-b8a2-f240d5e1c440/linked_in_profile'>IBM Cybersecurity Analyst Professional Certification</a>.
-                        I love coding and solving any and all challenging problems that come with it. 
-                        Check out the links below to contact me and to see some of my projects!
+                        I am also currently pursuing my CompTIA Security+ certification.
+                        <br/>My current tech stack includes Java, C#, C++, Python, HTML, CSS, JavaScript, & SQL.<br/>
+                        I'm also knowledgeable of the following libraries & frameworks: 
+                        Spring, Javalin, JDBC, JUnit, AWS, & (some) React. <br/>
+                        I am open to work as a Software Developer or a Security Analyst/Engineer/Architect. <br/>
+                        Check out the links below to contact me & to see some of my projects!
                     </Typography>
                 </CardContent>
                 <CardActions>
